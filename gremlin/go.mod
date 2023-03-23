1e97ecc3dc4280d16f5ac3ae4a24ccc793d03528
hangyudu <hangyudu@tencent.com>
2023-03-23 21:50:44 +0800

    [master][update]add dependency

--- gremlin/go.mod
+++ gremlin/go.mod
@@ -7,6 +7,7 @@ require github.com/apache/tinkerpop/gremlin-go v0.0.0-20220530191148-29272fa563e
 require (
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
-	github.com/nicksnyder/go-i18n/v2 v2.2.0 // indirect
-	golang.org/x/text v0.3.7 // indirect
+	github.com/nicksnyder/go-i18n/v2 v2.2.1 // indirect
+	github.com/stretchr/testify v1.8.1 // indirect
+	golang.org/x/text v0.5.0 // indirect
 )
--- gremlin/main.go
+++ gremlin/main.go
@@ -3,40 +3,93 @@ package main
 import (
 	"fmt"
 	gremlingo "github.com/apache/tinkerpop/gremlin-go/driver"
+	"log"
+	"regexp"
 )
 
 func main() {
-	// Creating the connection to the server with default settings.
-	driverRemoteConnection, err := gremlingo.NewDriverRemoteConnection("ws://localhost:8182/gremlin")
-	// Handle error
+	sql := "4||||insert"
+	sqlMatcherOne, err := regexp.Compile("^[0-9]+\\|\\|\\|\\|")
+	if err != nil {
+		fmt.Println("error")
+	}
+	// 部分SQL以[0-9]+||||的格式开头，后面紧跟一个正常的SQL
+	if ok := sqlMatcherOne.MatchString("4||||insert"); ok {
+		sql = sqlMatcherOne.ReplaceAllString(sql, "")
+	}
+}
+
+func gremlinConfigFunc() func(settings *gremlingo.DriverRemoteConnectionSettings) {
+	return func(settings *gremlingo.DriverRemoteConnectionSettings) {
+		settings.AuthInfo = &gremlingo.AuthInfo{
+			Username: "hetu",
+			Password: "Hetu@2022",
+		}
+		settings.InitialConcurrentConnections = 1
+	}
+}
+
+// queryProperties 查询traversal的所有properties，并根据id分组后返回
+func queryProperties(traversal *gremlingo.GraphTraversal) (error, map[string]map[string]string) {
+	resultMap := make(map[string]map[string]string)
+	traversalBackup := traversal.Clone()
+	resultSet, err := traversalBackup.ValueMap(true).GetResultSet()
 	if err != nil {
 		fmt.Println(err)
+		return err, nil
+	}
+	all, err := resultSet.All()
+	if err != nil {
+		fmt.Println(err)
+		return err, nil
+	}
+	for _, result := range all {
+		rawData := result.GetInterface()
+		data := rawData.(map[interface{}]interface{})
+		if id, ok := data["id"]; ok {
+			groupProperties := make(map[string]string)
+			for k, v := range data {
+				groupProperties[fmt.Sprintf("%v", k)] = fmt.Sprintf("%v", v)
+			}
+			resultMap[fmt.Sprintf("%v", id)] = groupProperties
+		}
+	}
+	return nil, resultMap
+}
+
+func query() {
+	// create a graph instance named driverRemoteConnection
+	driverRemoteConnection, err := gremlingo.NewDriverRemoteConnection("ws://43.138.47.95:10001/gremlin", gremlinConfigFunc())
+	if err != nil {
+		log.Fatalf("error create gremlin go connection caused by : %s", err)
 		return
 	}
-	// Cleanup
-	defer driverRemoteConnection.Close()
-
-	// Create an anonymous traversal source with remote
+	// create a TraversalSource provide gremlin context
 	g := gremlingo.Traversal_().WithRemote(driverRemoteConnection)
 
-	// Add a vertex with properties to the graph with the terminal step Iterate()
-	promise := g.AddV("gremlin").Property("language", "go").Iterate()
+	var id = "ieg_ngr_ods|ods_ngr_function_login_playerlogin_di"
 
-	// The returned promised is a go channel to wait for all submitted steps to finish execution and return error.
-	err = <-promise
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	traversal := g.V(id).BothE().Path().Unfold()
+	err, _ = queryProperties(traversal)
 
-	// Get the value of the property
-	result, err := g.V().HasLabel("gremlin").Values("language").ToList()
+	list, err := traversal.Clone().GetResultSet()
+
+	//g = r.V(vertexId).InE(edgeLabel).Path().Unfold()
+	result, _ := list.All()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	// Print the result
 	for _, r := range result {
-		fmt.Println(r.GetString())
+		fmt.Printf("%v", r)
+		//r.getI
+		//data := rawData.(map[interface{}]interface{})
+		//for k, v := range data {
+		//	fmt.Printf("%v = %v\n", k, v)
+		//}
 	}
+
+	driverRemoteConnection.Close()
+	return
 }
