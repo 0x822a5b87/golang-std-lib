7f664bb9054e33fb691c38644d3778042e7b683d
hangyudu <hangyudu@tencent.com>
2023-03-28 20:29:01 +0800

    [master][add]add hdfs example

---
+++ hdfs/go.mod
@@ -0,0 +1,17 @@
+module hangyudu/golang-std-lib/hdfs
+
+go 1.18
+
+require (
+	github.com/colinmarc/hdfs/v2 v2.3.0 // indirect
+	github.com/hashicorp/go-uuid v1.0.3 // indirect
+	github.com/jcmturner/aescts/v2 v2.0.0 // indirect
+	github.com/jcmturner/dnsutils/v2 v2.0.0 // indirect
+	github.com/jcmturner/gofork v1.7.6 // indirect
+	github.com/jcmturner/goidentity/v6 v6.0.1 // indirect
+	github.com/jcmturner/gokrb5/v8 v8.4.4 // indirect
+	github.com/jcmturner/rpc/v2 v2.0.3 // indirect
+	golang.org/x/crypto v0.7.0 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+)
---
+++ hdfs/hdfs.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/colinmarc/hdfs/v2"
+	"github.com/colinmarc/hdfs/v2/hadoopconf"
+	"os"
+	"os/user"
+	"path/filepath"
+)
+
+func main() {
+	address := os.Args[1]
+	path := os.Args[2]
+	conf := hadoopconf.HadoopConf{}
+	conf["fs.defaultFS"] = "hdfs://qy-ieg-4-v2"
+	conf["dfs.nameservices"] = "qy-ieg-4-v2"
+	conf["dfs.ha.namenodes.qy-ieg-4-v2"] = "nn1,nn2,nn3"
+	conf["dfs.namenode.rpc-address.qy-ieg-4-v2.nn1"] = "qy-ieg-4-v2-nn-1.tencent-distribute.com:9000"
+	conf["dfs.namenode.http-address.qy-ieg-4-v2.nn1"] = "qy-ieg-4-v2-nn-1.tencent-distribute.com:8080"
+	conf["dfs.namenode.rpc-address.qy-ieg-4-v2.nn2"] = "qy-ieg-4-v2-nn-2.tencent-distribute.com:9000"
+	conf["dfs.namenode.http-address.qy-ieg-4-v2.nn2"] = "qy-ieg-4-v2-nn-2.tencent-distribute.com:8080"
+	conf["dfs.namenode.rpc-address.qy-ieg-4-v2.nn3"] = "qy-ieg-4-v2-nn-3.tencent-distribute.com:9000"
+	conf["dfs.namenode.http-address.qy-ieg-4-v2.nn3"] = "qy-ieg-4-v2-nn-3.tencent-distribute.com:8080"
+	option := hdfs.ClientOptionsFromConf(conf)
+	option.Addresses = []string{address}
+	u, _ := user.Current()
+	option.User = u.Username
+	client, err := hdfs.NewClient(option)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	defer client.Close()
+
+	dir, err := client.ReadDir(path)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	for _, fileInfo := range dir {
+		func() {
+			p := filepath.Join(path, fileInfo.Name())
+			file, err := client.Open(p)
+			scanner := bufio.NewScanner(file)
+			// optionally, resize scanner's capacity for lines over 64K, see next example
+			for scanner.Scan() {
+				fmt.Println(scanner.Text())
+			}
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+			defer file.Close()
+		}()
+	}
+}
